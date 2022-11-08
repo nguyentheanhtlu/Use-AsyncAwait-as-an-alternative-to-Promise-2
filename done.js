4d6ca502f1fc02db6f5d7c50157360804e6e0a70
nguyentheanhtlu <theanh28022003@gmail.com>
2022-11-08 09:51:57 +0700

    done

---
+++ done.js
@@ -0,0 +1,18 @@
+async function onMyBirthDayAsync(isKayoSick){
+    if (!isKayoSick) return 2;
+    throw new Error('I am sad');
+}
+async function doSomethingAsync(){
+    try {
+        let result = await onMyBirthDayAsync(false);
+        console.log(`I have ${result} cakes`)
+    }
+    catch(error) {
+        console.log(error);
+    }
+    finally {
+        console.log("Party");
+    }
+}
+doSomethingAsync();
+
